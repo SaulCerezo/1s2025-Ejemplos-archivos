355b3fa9513693c8d2b639cee9cc33bcb651abe8
SaulCerezo <3643993410101@ingenieria.usac.edu.gt>
2025-02-18 21:10:35 -0600

    Particiones

---
+++ Ejemplo3l18-02-2025l/Gestion/Gestion.go
@@ -0,0 +1,281 @@
+package Gestion
+
+import (
+	"ejemplo2/Structs"
+	"ejemplo2/Utilities"
+	"encoding/binary"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func Mkdisk(size int, fit string, unit string, path string) {
+	fmt.Println("======INICIO MKDISK======")
+	fmt.Printf("Size: %d\nFit: %s\nUnit: %s\nPath: %s\n", size, fit, unit, path)
+
+	// Validaciones
+	if fit != "bf" && fit != "wf" && fit != "ff" {
+		fmt.Println("Error: Fit debe ser 'bf', 'wf' o 'ff'")
+		return
+	}
+	if size <= 0 {
+		fmt.Println("Error: Size debe ser mayor a 0")
+		return
+	}
+	if unit != "k" && unit != "m" {
+		fmt.Println("Error: Las unidades válidas son 'k' o 'm'")
+		return
+	}
+
+	// Crear archivo
+	if err := Utilities.CreateFile(path); err != nil {
+		fmt.Println("Error al crear archivo:", err)
+		return
+	}
+
+	// Convertir tamaño a bytes
+	sizeInBytes := size * 1024
+	if unit == "m" {
+		sizeInBytes *= 1024
+	}
+
+	// Abrir archivo
+	file, err := Utilities.OpenFile(path)
+	if err != nil {
+		fmt.Println("Error al abrir archivo:", err)
+		return
+	}
+	defer file.Close() // Asegura el cierre del archivo al salir de la función
+
+	// Escribir ceros en un solo bloque en lugar de un bucle
+	zeroBlock := make([]byte, sizeInBytes) // Crea un slice de bytes lleno de ceros
+	if _, err := file.Write(zeroBlock); err != nil {
+		fmt.Println("Error al escribir en el archivo:", err)
+		return
+	}
+
+	// Crear MBR
+	var newMBR Structs.MRB
+	newMBR.MbrSize = int32(sizeInBytes)
+	newMBR.Signature = rand.Int31()
+	copy(newMBR.Fit[:], fit)
+
+	// Obtener fecha actual en formato YYYY-MM-DD
+	formattedDate := time.Now().Format("2006-01-02")
+	copy(newMBR.CreationDate[:], formattedDate)
+
+	// Escribir el MBR en el archivo
+	if err := Utilities.WriteObject(file, newMBR, 0); err != nil {
+		fmt.Println("Error al escribir el MBR:", err)
+		return
+	}
+
+	// Leer el MBR para verificar que se escribió correctamente
+	var tempMBR Structs.MRB
+	if err := Utilities.ReadObject(file, &tempMBR, 0); err != nil {
+		fmt.Println("Error al leer el MBR:", err)
+		return
+	}
+
+	// Imprimir el MBR leído
+	Structs.PrintMBR(tempMBR)
+
+	fmt.Println("======FIN MKDISK======")
+}
+
+func Fdisk(size int, path string, name string, unit string, type_ string, fit string) {
+	fmt.Println("======Start FDISK======")
+	fmt.Println("Size:", size)
+	fmt.Println("Path:", path)
+	fmt.Println("Name:", name)
+	fmt.Println("Unit:", unit)
+	fmt.Println("Type:", type_)
+	fmt.Println("Fit:", fit)
+
+	// Validar fit (b/w/f)
+	if fit != "b" && fit != "f" && fit != "w" {
+		fmt.Println("Error: Fit must be 'b', 'f', or 'w'")
+		return
+	}
+
+	// Validar size > 0
+	if size <= 0 {
+		fmt.Println("Error: Size must be greater than 0")
+		return
+	}
+
+	// Validar unit (b/k/m)
+	if unit != "b" && unit != "k" && unit != "m" {
+		fmt.Println("Error: Unit must be 'b', 'k', or 'm'")
+		return
+	}
+
+	// Ajustar el tamaño en bytes
+	if unit == "k" {
+		size = size * 1024
+	} else if unit == "m" {
+		size = size * 1024 * 1024
+	}
+
+	// Abrir el archivo binario en la ruta proporcionada
+	file, err := Utilities.OpenFile(path)
+	if err != nil {
+		fmt.Println("Error: Could not open file at path:", path)
+		return
+	}
+
+	//Leer el MBR (Master Boot Record), Se
+	// lee el MBR desde el archivo para obtener la estructura del disco.
+	var TempMBR Structs.MRB
+	// Leer el objeto desde el archivo binario
+	if err := Utilities.ReadObject(file, &TempMBR, 0); err != nil {
+		fmt.Println("Error: Could not read MBR from file")
+		return
+	}
+
+	// Imprimir el objeto MBR
+	Structs.PrintMBR(TempMBR)
+
+	fmt.Println("-------------")
+
+	// Validaciones de las particiones
+	var primaryCount, extendedCount, totalPartitions int
+	var usedSpace int32 = 0
+
+	for i := 0; i < 4; i++ {
+		if TempMBR.Partitions[i].Size != 0 {
+			totalPartitions++
+			usedSpace += TempMBR.Partitions[i].Size
+
+			if TempMBR.Partitions[i].Type[0] == 'p' {
+				primaryCount++
+			} else if TempMBR.Partitions[i].Type[0] == 'e' {
+				extendedCount++
+			}
+		}
+	}
+
+	// Validar que no se exceda el número máximo de particiones primarias y extendidas
+	if totalPartitions >= 4 {
+		fmt.Println("Error: No se pueden crear más de 4 particiones primarias o extendidas en total.")
+		return
+	}
+
+	// Validar que solo haya una partición extendida
+	if type_ == "e" && extendedCount > 0 {
+		fmt.Println("Error: Solo se permite una partición extendida por disco.")
+		return
+	}
+
+	// Validar que no se pueda crear una partición lógica sin una extendida
+	if type_ == "l" && extendedCount == 0 {
+		fmt.Println("Error: No se puede crear una partición lógica sin una partición extendida.")
+		return
+	}
+
+	// Validar que el tamaño de la nueva partición no exceda el tamaño del disco
+	if usedSpace+int32(size) > TempMBR.MbrSize {
+		fmt.Println("Error: No hay suficiente espacio en el disco para crear esta partición.")
+		return
+	}
+
+	// Determinar la posición de inicio de la nueva partición
+	var gap int32 = int32(binary.Size(TempMBR))
+	if totalPartitions > 0 {
+		gap = TempMBR.Partitions[totalPartitions-1].Start + TempMBR.Partitions[totalPartitions-1].Size
+	}
+
+	// Encontrar una posición vacía para la nueva partición
+	for i := 0; i < 4; i++ {
+		if TempMBR.Partitions[i].Size == 0 {
+			if type_ == "p" || type_ == "e" {
+				// Crear partición primaria o extendida
+				//copy se usa para copiar el contenido de name
+				// (que es un string) a TempMBR.Partitions[i].Name, que es un array de bytes.
+				TempMBR.Partitions[i].Size = int32(size)
+				TempMBR.Partitions[i].Start = gap
+				copy(TempMBR.Partitions[i].Name[:], name)
+				copy(TempMBR.Partitions[i].Fit[:], fit)
+				copy(TempMBR.Partitions[i].Status[:], "0")
+				copy(TempMBR.Partitions[i].Type[:], type_)
+				TempMBR.Partitions[i].Correlative = int32(totalPartitions + 1)
+
+				if type_ == "e" {
+					// Inicializar el primer EBR en la partición extendida
+					ebrStart := gap // El primer EBR se coloca al inicio de la partición extendida
+					ebr := Structs.EBR{
+						PartFit:   fit[0],
+						PartStart: ebrStart,
+						PartSize:  0,
+						PartNext:  -1,
+					}
+					copy(ebr.PartName[:], "")
+					Utilities.WriteObject(file, ebr, int64(ebrStart))
+				}
+
+				break
+			}
+		}
+	}
+
+	// Manejar la creación de particiones lógicas dentro de una partición extendida
+	if type_ == "l" {
+		for i := 0; i < 4; i++ {
+			if TempMBR.Partitions[i].Type[0] == 'e' {
+				ebrPos := TempMBR.Partitions[i].Start
+				var ebr Structs.EBR
+				for {
+					Utilities.ReadObject(file, &ebr, int64(ebrPos))
+					if ebr.PartNext == -1 {
+						break
+					}
+					ebrPos = ebr.PartNext
+				}
+
+				// Calcular la posición de inicio de la nueva partición lógica
+				newEBRPos := ebr.PartStart + ebr.PartSize                    // El nuevo EBR se coloca después de la partición lógica anterior
+				logicalPartitionStart := newEBRPos + int32(binary.Size(ebr)) // El inicio de la partición lógica es justo después del EBR
+
+				// Ajustar el siguiente EBR
+				ebr.PartNext = newEBRPos
+				Utilities.WriteObject(file, ebr, int64(ebrPos))
+
+				// Crear y escribir el nuevo EBR
+				newEBR := Structs.EBR{
+					PartFit:   fit[0],
+					PartStart: logicalPartitionStart,
+					PartSize:  int32(size),
+					PartNext:  -1,
+				}
+				copy(newEBR.PartName[:], name)
+				Utilities.WriteObject(file, newEBR, int64(newEBRPos))
+
+				// Imprimir el nuevo EBR creado
+				fmt.Println("Nuevo EBR creado:")
+				Structs.PrintEBR(newEBR)
+				break
+			}
+		}
+	}
+
+	// Sobrescribir el MBR
+	if err := Utilities.WriteObject(file, TempMBR, 0); err != nil {
+		fmt.Println("Error: Could not write MBR to file")
+		return
+	}
+
+	var TempMBR2 Structs.MRB
+	// Leer el objeto nuevamente para verificar
+	if err := Utilities.ReadObject(file, &TempMBR2, 0); err != nil {
+		fmt.Println("Error: Could not read MBR from file after writing")
+		return
+	}
+
+	// Imprimir el objeto MBR actualizado
+	Structs.PrintMBR(TempMBR2)
+
+	// Cerrar el archivo binario
+	defer file.Close()
+
+	fmt.Println("======FIN FDISK======")
+}
